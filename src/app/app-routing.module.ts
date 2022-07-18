df7183e2b85a14c8b7c4bf45d78734a53e56df98
jeiss <jeissoncortes02@outlook.com>
2022-07-18 09:14:00 -0500

    configuracion rutas y componentes

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router'; // CLI imports router
+import { PorCapitalComponent } from './pais/pages/por-capital/por-capital.component';
+import { PorPaisComponent } from './pais/pages/por-pais/por-pais.component';
+import { PorRegionComponent } from './pais/pages/por-region/por-region.component';
+import { VerPaisComponent } from './pais/pages/ver-pais/ver-pais.component';
+
+const routes: Routes = [
+    {
+        path:'', component: PorPaisComponent, pathMatch: 'full'
+    },
+    {
+        path: 'region', component: PorRegionComponent
+    },
+    {
+        path: 'capital', component: PorCapitalComponent
+    },
+    {
+        path: 'pais/:id', component: VerPaisComponent, 
+    },
+    {
+        path: '**', redirectTo: ''
+    }
+]; // sets up routes constant where you define your routes
+
+// configures NgModule imports and exports
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
