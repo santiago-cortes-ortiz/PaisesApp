7c4e6b55bacbe53eeb93e96b2d0d8076ce0bd3cd
jeiss <jeissoncortes02@outlook.com>
2022-07-27 16:56:25 -0500

    manejo del subscribe para la busqueda

--- src/app/pais/pages/por-pais/por-pais.component.ts
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -9,16 +9,19 @@ import { PaisService } from '../../services/pais.service';
 export class PorPaisComponent{
 
   termino: string = '';
+  error: boolean = false;
 
   constructor(private paisService: PaisService) {
   }
 
   buscar(){
+    this.error = false;
     console.log(this.termino)
     this.paisService.buscarPais(this.termino)
     .subscribe(
-      response => {
-        console.log(response)
+      {
+        next: (response) => {console.log(response)},
+        error: (err) => { this.error = true}
       }
     );
   }
